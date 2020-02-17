ca362a5317f5ffc1c3b4612fa2a25df746b35b7e
Julianna Adler-Colvin <julianna.adler-colvin1@marist.edu>
2020-02-17 17:48:00 -0500

    More usability improvements to the guessing game

--- guessing-game.py
+++ guessing-game.py
@@ -4,14 +4,25 @@ def main():
     animal = "axolotl"
     zero = 0
     while zero == 0:
+        print("I am thinking of an animal.")
         guess = input("Guess what animal I am thinking of.")
         if guess.lower() == animal:
             zero += 1
             print("Congrats! You've guessed the animal.")
-        elif guess.lower() == "quit":
+            Theaxolotl = 0
+            while Theaxolotl == 0:
+                response = input("Do you like this animal? Y/N: ")
+                if response[0].lower() == "n":
+                    print("Boo. The axolotl is a great animal!")
+                    Theaxolotl += 1
+                elif response [0].lower() == "y":
+                    print("Great! I love this animal too.")
+                    Theaxolotl += 1
+        elif guess[0].lower() == "q":
             zero += 1
         else:
             print("Try again. Wrong animal.")
+    print ("Play again!")
 main()
 
             
